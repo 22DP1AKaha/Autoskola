91a19c5faa1f2cd99337ca99e341552bcd4aea9d
22DP1AKaha <131352892+22DP1AKaha@users.noreply.github.com>
2024-03-06 11:45:29 +0000

    commit

--- src/main/java/com/example/skola/AutoSkolaApplication.java
+++ src/main/java/com/example/skola/AutoSkolaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkolaApplication {
+public class AutoSkolaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkolaApplication.class, args);
+		SpringApplication.run(AutoSkolaApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/skola/MainController.java
@@ -0,0 +1,13 @@
+package com.example.skola;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController
+{
+    @GetMapping(value = "/")
+    public String indexPage() {
+        return "index";
+    }
+}
